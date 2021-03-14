fa57d592f4e1839a91da5588acf0279d194a3981
Matthew Ao <aorui@MacBook-Air.local>
2021-03-14 14:47:13 -0400

    Changed API call to use fetch instead of xmlhttprequest

--- server/src/ts/games/game.ts
+++ server/src/ts/games/game.ts
@@ -21,9 +21,10 @@ import * as Round from "./game/round";
 import * as PublicRound from "./game/round/public";
 import * as Player from "./player";
 import * as Rules from "./rules";
+import fetch from "node-fetch";
 //import fetch from "node-fetch";
 // eslint-disable-next-line prettier/prettier
-import { XMLHttpRequest } from 'xmlhttprequest-ts';
+//import { XMLHttpRequest } from 'xmlhttprequest-ts';
 
 export interface Public {
   round: PublicRound.Public;
@@ -468,7 +469,7 @@ export class Game {
       console.log("POTENTIAL PLAYS _____________________________");
       console.log(potentialPlays);
 
-      /* Console log for debugging purposes*/
+      /* Console log for debugging purposes
       console.log("______________________________________");
       console.log("Call: ");
       console.log(game.round.call["parts"]);
@@ -478,8 +479,140 @@ export class Game {
       console.log(player.hand);
       console.log("Potential plays: ");
       console.log(JSON.stringify(potentialPlays));
-      console.log("______________________________________");
+      console.log("______________________________________");*/
 
+      //const formData = { plays: ["string1", "string2"] };
+      const key = "plays";
+      const value = JSON.stringify(potentialPlays);
+      const formString = '{"' + key + '":' + value + "}";
+      const formData = JSON.parse(formString);
+      /*const formBody = [];
+      const encodedKey = encodeURIComponent("text");
+      const encodedValue = encodeURIComponent(JSON.stringify(potentialPlays));
+      formBody.push(encodedKey + "=" + encodedValue);
+      const form = formBody.join("&");
+*/
+      let responseJson;
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      async function getResponse(formData: string) {
+        const response = await fetch("http://44.230.29.224/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: formData,
+        });
+        console.log(response.status);
+        const resJson = await response.json();
+        responseJson = resJson;
+        return resJson;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      async function result(formData: string) {
+        const json = await getResponse(formData);
+        responseJson = json; // this will get set and work however this will not happen before line 35 gets executed
+        console.log("a", json); // this will work
+        console.log("b", responseJson); // and so will this
+
+        pickCard(responseJson);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      async function pickCard(responseJson: unknown) {
+        const stringData = JSON.stringify(responseJson);
+        const parse = JSON.parse(stringData);
+        console.log(parse);
+
+        let maxValue = 0;
+
+        // Find the max score from API response data
+        for (const x in parse) {
+          if (Number(parse[x]) > Number(maxValue)) {
+            maxValue = parse[x];
+          }
+        }
+
+        console.log("Max Score: " + Number(maxValue));
+
+        // Get the top play from the API response data
+        const topPlay = String(getKeyByValue(parse, maxValue));
+        console.log("Top Play: " + topPlay);
+
+        // Iterate through player's hand and identify the top plays
+        // and store their indeces in an array, indecesOfTopPlays
+        const indecesOfTopPlays = [];
+        for (const card in player.hand) {
+          if (player.hand.hasOwnProperty(card)) {
+            const potentialPlay = player.hand[card]["text"]
+              .slice(0, player.hand[card]["text"].length)
+              .replace(/['"]+/g, "");
+            const indexOfTopPlay = topPlay.indexOf(potentialPlay);
+            if (indexOfTopPlay > -1) {
+              console.log(potentialPlay + " is located at " + indexOfTopPlay);
+              // store the potential play
+              indecesOfTopPlays.push([indexOfTopPlay, Number(card)]);
+            }
+          }
+        }
+
+        // Sort top play indeces by order in which they will be played
+        indecesOfTopPlays.sort(sortByPlay);
+
+        // Copy top plays to new array
+        const topPlays = [];
+        for (let i = 0; i < indecesOfTopPlays.length; i++) {
+          topPlays.push(player.hand[indecesOfTopPlays[i][1]]);
+        }
+
+        console.log("TOP PLAYS: ");
+        console.log(topPlays);
+
+        // If there is a top play in the player's hand, proceed to play it
+        if (topPlays.length > 0) {
+          console.log("Playing top play");
+          // Delete top plays from player hand
+          indecesOfTopPlays.sort(sortByDeletion);
+          for (let i = 0; i < indecesOfTopPlays.length; i++) {
+            player.hand.splice(indecesOfTopPlays[i][1], 1);
+          }
+
+          // Append top plays to beginning of player hand
+          player.hand.splice(0, 0, ...topPlays);
+          // Delete top plays from player hand
+          indecesOfTopPlays.sort(sortByDeletion);
+          for (let i = 0; i < indecesOfTopPlays.length; i++) {
+            player.hand.splice(indecesOfTopPlays[i][1], 1);
+          }
+
+          // Append top plays to beginning of player hand
+          player.hand.splice(0, 0, ...topPlays);
+        } else {
+          console.log("Playing random card");
+        }
+
+        console.log("FINAL PLAYER HAND: ");
+        console.log(player.hand);
+
+        // Play card
+        const play = player.hand.slice(0, slotCount) as Card.Response[];
+
+        plays.push({
+          id: playId,
+          play: play,
+          playedBy: ai,
+          revealed: false,
+          likes: [],
+        });
+        events.push(Event.targetAll(PlaySubmitted.of(ai)));
+      }
+
+      result(JSON.stringify(formData));
+
+      //console.log("c", responseJson); // prints nothing because this is run immediately after result()
+
+      /*
       const fetchAPI: VoidFunction = function () {
         // API call to top score calculator
         let responseText;
@@ -532,7 +665,7 @@ export class Game {
               console.log("Top Play: " + topPlay);
 
               // Iterate through player's hand and identify the top plays
-              // and store their indeces in an array, inde cesOfTopPlays
+              // and store their indeces in an array, indecesOfTopPlays
               const indecesOfTopPlays = [];
               for (const card in player.hand) {
                 if (player.hand.hasOwnProperty(card)) {
@@ -564,8 +697,6 @@ export class Game {
 
               // Delete top plays from player hand
               indecesOfTopPlays.sort(sortByDeletion);
-              //console.log(indecesOfTopPlays);
-
               for (let i = 0; i < indecesOfTopPlays.length; i++) {
                 player.hand.splice(indecesOfTopPlays[i][1], 1);
               }
@@ -576,7 +707,7 @@ export class Game {
               console.log("FINAL PLAYER HAND: ");
               console.log(player.hand);
 
-              // Play random card
+              // Play card
               const play = player.hand.slice(0, slotCount) as Card.Response[];
 
               plays.push({
@@ -624,6 +755,7 @@ export class Game {
         likes: [],
       });
       events.push(Event.targetAll(PlaySubmitted.of(ai)));
+      */
     }
 
     const timeouts = [];
